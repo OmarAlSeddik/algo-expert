c4e1d3b49c0b60b6deb0dcb2e007abe1de96f632
unknown <omaralseddik@gmail.com>
2022-09-12 23:28:37 +0200

    monotonic-array

---
+++ Arrays/monotonic-array.ts
@@ -0,0 +1,22 @@
+const isMonotonic = (array: number[]) => {
+  if (array.length < 2) return true;
+  let increasing: any = undefined;
+  for (let i = 0; i < array.length; i++) {
+    if (i > 0) {
+      switch (increasing) {
+        case true: {
+          if (array[i] < array[i - 1]) return false;
+          break;
+        }
+        case false: {
+          if (array[i] > array[i - 1]) return false;
+        }
+        default: {
+          if (array[i] > array[i - 1]) increasing = true;
+          if (array[i] < array[i - 1]) increasing = false;
+        }
+      }
+    }
+  }
+  return true;
+};
