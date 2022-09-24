80463e3a49289c0d8b3e84e3a8485c63c6496ede
unknown <omaralseddik@gmail.com>
2022-09-24 23:20:06 +0200

    insertion-sort

---
+++ Sorting/insertion-sort.ts
@@ -0,0 +1,12 @@
+const insertionSort = (array: number[]) => {
+  for (let i = 0; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      let temp = array[j];
+      array[j] = array[j - 1];
+      array[j - 1] = temp;
+      j -= 1;
+    }
+  }
+  return array;
+};
