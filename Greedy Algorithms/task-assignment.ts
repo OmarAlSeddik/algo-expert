7beea37391739f8ce6f68ef590201783bb651c46
unknown <omaralseddik@gmail.com>
2022-10-07 23:39:56 +0200

    task-assignment

---
+++ Greedy Algorithms/task-assignment.ts
@@ -0,0 +1,12 @@
+const taskAssignment = (k: number, tasks: number[]) => {
+  let solution: number[][] = [];
+  const newTasks = tasks.map((n, i) => [n, i]).sort((a, b) => a[0] - b[0]);
+  let left = 0;
+  let right = tasks.length - 1;
+  while (left < right) {
+    solution.push([newTasks[left][1], newTasks[right][1]]);
+    left++;
+    right--;
+  }
+  return solution;
+};
